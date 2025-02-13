f24dfba88e10c7f10462f928561dcbe29ea9edfa
AnDev58 <andev58@outlook.com>
2025-02-13 17:23:25 +0300

    added portal texture and timer

--- python/main.py
+++ python/main.py
@@ -4,6 +4,7 @@ import pygame as pg
 
 from map import Map
 from portal import Portal
+from countdown_timer import Timer
 from state import State
 from landscape import Landscape
 from camera import Camera
@@ -19,6 +20,8 @@ if __name__ == '__main__':
     
     portal = Portal(250, 200, state)
 
+    timer = Timer(1, screen, state)
+
     landscape = Landscape(screen, state, maps, state.MAP_RESOLUTION, portal)
     
     running = True
@@ -26,12 +29,16 @@ if __name__ == '__main__':
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
+            elif event.type == timer.EVENT:
+                timer.update()
 
         state.camera.update()
 
         landscape.terraforming()
         landscape.update()
         landscape.render()
+
+        timer.draw()
         
         if landscape.check_win(portal):
             print("Win")
